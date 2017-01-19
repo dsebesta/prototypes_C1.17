9f18b11504f134149f40d34858ca78ef93116405
Derrick Sebesta <dsebesta@gmail.com>
2017-01-19 15:29:10 -0800

    js event delegation - Derrick Sebesta

--- js_event_delegation/script.js
+++ js_event_delegation/script.js
@@ -1,10 +1,42 @@
 // Answer the questions here:
 
 // - What do you think is going to happen?
-
+//      Going to show the text
 // - What happened?
-
+//      It did not show the text
 // - Why?
-
+//      I do not know why. Maybe because the code was executed against button elements before our button element was created.
 //========== Write your code below ===========//
 
+$(document).ready(function (){
+
+    // $('#list button').on('click', function() { ****** Original Code *******
+
+    $('#list').on('click', 'button', function() {
+        console.log($(this).text());
+        var attr_test = $(this).attr('say_what');
+        if (attr_test == 'choose_only_me') {
+            window.open('http://www.google.com', '_blank')
+        }
+    });
+
+    var li_element = $('<li>');
+    var button_element = $('<button>', {
+       style: 'margin-top: 10px',
+        text: 'Delegated Button#5 Handler'
+    });
+
+    $('#list').append(li_element);
+    $(li_element).append(button_element);
+
+    var li_element_2 = $('<li>');
+    var button_element_2 = $('<button>', {
+        say_what: 'choose_only_me',
+        text: "CLICK ME CLICK ME!",
+        style: 'margin-top: 10px'
+    })
+
+    $('#list').append(li_element_2);
+    $(li_element_2).append(button_element_2);
+
+});
