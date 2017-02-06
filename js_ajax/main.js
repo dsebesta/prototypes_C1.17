cffff4b3138b940cc9803e6aa50e60d848b0ebfc
Derrick Sebesta <dsebesta@gmail.com>
2017-02-06 14:09:08 -0800

    js ajax - Derrick Sebesta

--- js_ajax/main.js
+++ js_ajax/main.js
@@ -1,5 +1,5 @@
 //Create GLOBAL variable below here on line 2
-
+var global_result;
 
 $(document).ready(function(){
     $('button').click(function(){
@@ -10,6 +10,23 @@ $(document).ready(function(){
             success: function(result) {
                 console.log('AJAX Success function called, with the following result:', result);
 
+                global_result = result;
+
+                for (var i=0; i < global_result.feed.entry.length; i++) {
+                    var movie_third_image = global_result.feed.entry[i]['im:image'][2].label;
+                    var movie_director = global_result.feed.entry[i]['im:artist'].label;
+                    var movie_title = global_result.feed.entry[i]['im:name'].label;
+
+                    var div_element = $('<div>');
+                    var movie_third_image_element = $('<img>', {
+                        src: movie_third_image
+                    });
+                    var movie_info_element = $('<p>' + 'Title: ' + movie_title + '<br>' + 'Director: ' + movie_director + '</p>');
+
+                    div_element.append(movie_third_image_element, movie_info_element);
+                    $('#main').append(div_element);
+
+                }
             }
         });
         console.log('End of click function');
