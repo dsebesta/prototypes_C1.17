a32ca2503bc7d255286979aa8347fa763915eb27
Derrick Sebesta <dsebesta@gmail.com>
2017-02-15 12:51:43 -0800

    PHP Includes - Derrick Sebesta

---
+++ php_includes/main.php
@@ -0,0 +1,10 @@
+<main id="main_content">
+    <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque malesuada euismod magna, vel iaculis ante
+        porttitor in. Duis iaculis imperdiet ex, non aliquet erat posuere a. Duis convallis risus id leo lacinia mollis.
+        Quisque ipsum nibh, ultricies ut faucibus id, blandit ut neque. Phasellus in blandit urna. Phasellus non purus in
+        urna ullamcorper vulputate. Aenean finibus nunc orci, vel congue augue tempus eu. Mauris vestibulum mauris sem, eu
+        bibendum orci bibendum non.
+    </div>
+    <img src="http://i.imgur.com/OFYhsP3.png">
+</main>
