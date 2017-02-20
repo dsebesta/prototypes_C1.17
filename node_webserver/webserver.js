0ffa214d961bc78028b38df2a8db2521378c7974
Derrick Sebesta <dsebesta@gmail.com>
2017-02-19 19:34:46 -0800

    version .7

--- node_webserver/webserver.js
+++ node_webserver/webserver.js
@@ -1,10 +1,62 @@
 var http = require('http');
+var fs = require('fs');
+var path = require('path');
 var server = http.createServer(handleRequest);
 
 function handleRequest(request, response) {
     console.log('handling request');
-    response.write('initializing request response \n');
-    response.end('It works! Path hit: ' + request.url)
+    var filePath = './doc_root' + request.url;
+    if (filePath === './doc_root/') {
+        filePath = './doc_root/index.html';
+    }
+
+    var extname = path.extname(filePath);
+    var statusCode = 200;
+    switch(extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.wav':
+            contentType = 'audio/wav';
+            break;
+        case 'ico':
+            contentType = 'image/x-icon';
+            break;
+        default:
+            contentType = 'text/html';
+            break;
+    }
+
+    if (fs.existsSync(filePath)) {
+        console.log(filePath + ' file exists')
+    } else {
+        console.log(filePath + ' file does not exist');
+        filePath = './doc_root/404.html';
+        statusCode = 404;
+
+    }
+
+    fs.readFile(filePath, function(error, data) {
+        if (error) {
+            console.log(data);
+        }
+        else {
+            response.writeHead(statusCode, { 'Content-Type': contentType });
+            response.end(data, 'utf-8');
+        }
+    })
 }
 
 server.listen(6133, function() {
