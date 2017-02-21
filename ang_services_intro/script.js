efe6fc89486e103c13b2ae1f62d6d2781329f0ef
Derrick Sebesta <dsebesta@gmail.com>
2017-02-21 13:15:05 -0800

    Ang Services Intro - Derrick Sebesta

--- ang_services_intro/script.js
+++ ang_services_intro/script.js
@@ -5,20 +5,22 @@ app.controller('formController', formFunction);
 function formFunction ($log, $http) {
     var self = this;
     self.artist = '';
+    self.results = [];
     self.createURL = function () {
-        self.url = 'https://itunes.apple.com/search?term=' + self.artist + '&callback=JSON_CALLBACK';
-        // self.url = 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json';
-        return self.url
+        var url = 'https://itunes.apple.com/search?term=' + self.artist + '&callback=JSON_CALLBACK';
+        return url;
     };
 
     self.retrieveData = function () {
         $http({
-            type: 'jsonp',
-            url: 'https://itunes.apple.com/search?term=derrick&callback=JSON_CALLBACK',
+            method: 'jsonp',
+            url: self.createURL(),
             cache: false
         }).then (
             function successCallback(response) {
-                $log.log('success: ', response)
+                $log.log('success: ', response);
+                self.results = response.data.results;
+                $log.log('results: ', self.results);
             },
             function errorCallback(response) {
                 $log.log('failed: ', response)
